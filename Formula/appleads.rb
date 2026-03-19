e80f69e1609c963f161ec65d2d124ae4856b3687
Ferdi kızıltoprak <ferdikt@gmail.com>
2026-03-19 16:46:06 +0300

    appleads 1.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/appleads.rb
+++ Formula/appleads.rb
@@ -1,13 +1,13 @@
 class Appleads < Formula
   desc "Production-ready CLI for Apple Search Ads API"
   homepage "https://github.com/FerdiKT/appleads-cli"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/FerdiKT/appleads-cli/releases/download/v#{version}/appleads_#{version}_darwin_arm64.tar.gz"
-      sha256 "3f2aca708aeb4e864a409664f916913306c18474db854f845036f6189521b4e3"
+      url "https://github.com/FerdiKT/appleads-cli/releases/download/v1.0.1/appleads_1.0.1_darwin_arm64.tar.gz"
+      sha256 "4ed40473bf42eeeb27b10d9d6f6109452d48f45f8be6e27f558de83c9dd5e4ca"
     else
       url "https://github.com/FerdiKT/appleads-cli/releases/download/v#{version}/appleads_#{version}_darwin_amd64.tar.gz"
       sha256 "d3f9b5063e8f1d7fd360182440a98b7a70ea6ca0d4d3b1f4316d3f1f358a1f8e"
