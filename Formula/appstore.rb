d49acd119d39c92cac8326f8b8e879c448c25ce6
Ferdi kızıltoprak <ferdikt@gmail.com>
2026-05-01 04:46:43 +0300

    appstore 0.1.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/appstore.rb
+++ Formula/appstore.rb
@@ -1,13 +1,13 @@
 class Appstore < Formula
   desc "CLI for direct App Store API search/hints/app-details"
   homepage "https://github.com/FerdiKT/appstore-cli"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/FerdiKT/appstore-cli/releases/download/v0.1.2/appstore_0.1.2_darwin_arm64.tar.gz"
-      sha256 "ddb17b7339be18f352a081190f2059e5b98e7164396232bc8f5142bc38e93e31"
+      url "https://github.com/FerdiKT/appstore-cli/releases/download/v0.1.3/appstore_0.1.3_darwin_arm64.tar.gz"
+      sha256 "11f89bdc5c9eb87c42ea398a86aa05c3bcd10876b5f1abb91aa2e3d3ba8fc689"
     else
       url "https://github.com/FerdiKT/appstore-cli/releases/download/v#{version}/appstore_#{version}_darwin_amd64.tar.gz"
       sha256 "a63d94236608e3be50a2d84df980e82e9230f08afbf4e9cbcbe56f50c589dd68"
